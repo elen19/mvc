37d6d7bf79568f43ebd21f9a060d9bdd53718bcc
Elis <elen19@student.bth.se>
2023-05-30 15:58:45 +0200

    Gjort tester och fixat lite style, vill testa scrut

--- tests/Card/CardHandTest.php
+++ tests/Card/CardHandTest.php
@@ -95,4 +95,78 @@ class CardHandTest extends TestCase
         $exp = true;
         $this->assertEquals($exp, $res);
     }
+
+    /**
+     * Test bet functions
+     */
+    public function testBet(): void {
+        $guess = new CardHand();
+
+        $res = $guess->getBet();
+        $exp = 0;
+        $this->assertEquals($exp, $res);
+
+        $guess->setBet(10);
+        $res = $guess->getBet();
+        $exp = 10;
+        $this->assertEquals($exp, $res);
+
+        $guess = new CardHand();
+        $guess->placeBet(10);
+        $res = $guess->getBet();
+        $exp = 10;
+        $this->assertEquals($exp, $res);
+        $res = $guess->getMoney();
+        $exp = 90;
+        $this->assertEquals($exp, $res);
+        $res = $guess->placeBet(300);
+        $exp = false;
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Test money functions
+     */
+    public function testMoney(): void {
+        $guess = new CardHand();
+
+        $res = $guess->getMoney();
+        $exp = 100;
+        $this->assertEquals($exp, $res);
+
+        $guess->setMoney(50);
+        $res = $guess->getMoney();
+        $exp = 50;
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Test winnings function
+     */
+    public function testWinning(): void {
+        $guess = new CardHand();
+
+        $guess->placeBet(100);
+        $guess->calculateWinnings(1.5);
+        $res = $guess->getMoney();
+        $exp = 150;
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Test clear hand function
+     */
+    public function testClearHand(): void {
+        $guess = new CardHand();
+        $card = new CardGraphic("A", "C");
+        $guess->addCard($card);
+        $guess->stay();
+        $guess->clearHand();
+        $res = $guess->getCards();
+        $exp = [];
+        $this->assertEquals($exp, $res);
+        $res = $guess->getStay();
+        $exp = false;
+        $this->assertEquals($exp, $res);
+    }
 }
